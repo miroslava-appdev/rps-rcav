49f3cbcb84f2626fe73282fdd7f1c3ad24b83459
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-04-12 05:45:29 +0000

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -14,6 +14,16 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
     render({ :template => "game_templates/user_rock.html.erb" })
   end
 
@@ -29,5 +39,19 @@ class ApplicationController < ActionController::Base
     end
 
     render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
 end
